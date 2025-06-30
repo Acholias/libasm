be4db15eff9e0c4a413f01e9b898d17a0fa9fd3b
Acholias <mugot.lucas@gmail.com>
2025-06-30 17:28:43 +0200

    Add errno for ft_write ;) Let's go call malloc

--- ft_write.s
+++ ft_write.s
@@ -3,10 +3,22 @@
 
 section .note.GNU-stack
 
+extern	__errno_location
+
 global	ft_write
 
 section .text
 ft_write:
 	mov			rax, 1
 	syscall
+	cmp			rax, 0
+	jge			.end
+	neg			rax
+	push		rax
+	call		__errno_location
+	pop			rdi
+	mov			[rax], edi
+	mov			rax, -1
+
+.end:
 	ret
