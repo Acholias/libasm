9ba491ff4bd21ccb8beac5fc3be888ef55cc1d90
Acholias <lucas.mugot@gmail.com>
2025-08-02 19:01:48 +0200

    Comment my bonus

--- ft_list_push_front.s
+++ ft_list_push_front.s
@@ -24,3 +24,19 @@ ft_list_push_front:
 
 .end:
 	ret
+
+
+; 1. regarder si **lst != NULL
+; 2. return si **lst == NULL
+; 3. regarder si data != NULL
+; 4. return si data == NULL
+; 5. Sauvegarder rdi (**lst) dans r8
+; 6. Sauvegarder rsi (data) dans r9
+; 7. Preparer rdi pour le call a malloc (rdi = 16)
+; 8. faire le malloc de la bonne taille (malloc(16))
+; 9. Faire la verif si le malloc echoue
+; 10. return si malloc a fail
+; 11. Mettre la valeur de data dans new->data
+; 12. Sauvegarder le *lst dans r10 pour le mettre dans next apres (Pas 2 instruction mémoire sur la meme ligne)
+; 13. Mettre l'ancienne tête (*lst) dans new->next pour chaîner les maillons (new->next = ancienne tete)
+; 14. Mettre le nouveau maillon (rax) comme nouvelle tête de liste (*lst = nouveau_maillon)"
--- ft_list_size.s
+++ ft_list_size.s
@@ -16,3 +16,12 @@ ft_list_size:
 
 .end:
 	ret
+
+
+1. Mettre le registre rax a 0
+2. Regarder si rdi (*lst) est NULL
+3. Si oui return (je -> jump-if-equal)
+4. Incrementer rax
+5. Faire avancer le pointeur dans la liste (rdi = rdi->next)
+6. Regarder si on est a la fin de la chaine
+7. Jump sur la loop si on est pas a la fin de la chaine (jne -> jump-if-no-equal)
