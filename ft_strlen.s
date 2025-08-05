ad72215f67310d06e2f18045e775483b12a4802d
Acholias <lucas.mugot@gmail.com>
2025-08-05 14:26:27 +0200

    ft_strlen with sub pointer

--- ft_strlen.s
+++ ft_strlen.s
@@ -14,3 +14,18 @@ ft_strlen:
 
 .end:
     ret
+
+
+
+; ft_strlen:
+; 	mov		rax, rdi
+; 
+; .loop:
+; 	cmp		byte [rax], 0
+; 	je		.end
+; 	inc		rax
+; 	jmp		.loop
+; 
+; .end:
+; 	sub		rax, rdi
+; 	ret
